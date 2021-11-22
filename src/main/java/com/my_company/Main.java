ce71085b99cb285115810b5d54a51be745b92165
OTretyakova <OTretyakova@beeline.kz>
2021-11-22 14:44:58 +0600

    Add methods for work with Collections

--- src/main/java/com/my_company/Main.java
+++ src/main/java/com/my_company/Main.java
@@ -1,8 +1,6 @@
 package com.my_company;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static com.my_company.Point.xComparator;
 import static com.my_company.Point.yComparator;
@@ -34,5 +32,61 @@ public class Main {
         for (Point p : points) {
             System.out.println(p);
         }
+
+        //List methods
+        Point new_point = new Point(1, 7);
+        points.remove(4);
+        points.contains(3);
+        points.set(3, new_point);
+        points.isEmpty();
+        points.size();
+        points.clear();
+
+        //Map example
+        Map<Integer, String> menu = new HashMap<>();
+        menu.put(1, "pizza");
+        menu.put(2, "fries");
+        menu.put(3, "pasta");
+        menu.put(4, "chicken");
+
+        //Map methods
+        Iterator<Map.Entry<Integer, String>> it = menu.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Integer, String> entry = it.next();
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " " + value);
+        }
+
+        menu.remove(2);
+        menu.containsValue("pizza");
+        menu.size();
+        menu.clear();
+        menu.isEmpty();
+
+        //Set example
+        Set<String> cities = new HashSet<>();
+
+        cities.add("Moscow");
+        cities.add("Piter");
+        cities.add("Novosibirsk");
+        cities.add("Ekaterinburg");
+
+        //Set methods
+        cities.remove("Ekaterinburg");
+        cities.contains("Piter");
+        cities.size();
+        cities.isEmpty();
+
+        Set<String> new_cities = new HashSet<>();
+        new_cities.add("Tomsk");
+        new_cities.add("Tyumen");
+
+        cities.addAll(new_cities);
+        Iterator<String> itr = cities.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
     }
 }
